82e90cd58eff0d5f237572c75089455c4c0ea0d0
Sameer <sameerbagwan132@gmail.com>
2023-08-24 20:25:34 +0530

    authSlice

---
+++ views/src/app/features/auth/authSlice.js
@@ -0,0 +1,19 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+  userInfo: localStorage.getItem('userInfo')
+    ? JSON.parse(localStorage.getItem('userInfo'))
+    : null,
+};
+
+const authSlices = createSlice({
+  name: 'auth',
+  initialState,
+  reducers: {
+    setCredentials: (state, action) => {
+      state.userInfo = action.payload;
+      localStorage.setItem('userInfo', JSON.stringify(action.payload));
+    },
+  },
+  extraReducers: {},
+});

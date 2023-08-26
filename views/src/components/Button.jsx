f1031bccc432dcc1ba9da08764f4848061806ce2
Sameer <sameerbagwan132@gmail.com>
2023-08-26 20:55:10 +0530

    update profile

--- views/src/components/Button.jsx
+++ views/src/components/Button.jsx
@@ -5,6 +5,7 @@ const Button = ({ isLoading, title, onClick }) => {
     <button
       type='submit'
       className='w-full text-white bg-blue-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800'
+      onClick={onClick}
     >
       {isLoading ? <LoadingView /> : title}
     </button>

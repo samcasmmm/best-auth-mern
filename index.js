32185598e75ff2adf9145377d084c75d5a80694a
Sameer <sameerbagwan132@gmail.com>
2023-08-27 18:09:49 +0530

    update profile

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 dotenv.config();
 import cookieParser from 'cookie-parser';
+import cors from 'cors';
 
 // file imports -
 import connectDB from './config/database.js';
@@ -17,6 +18,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.use(cors());
 
 // route
 app.get('/', (req, res) => {

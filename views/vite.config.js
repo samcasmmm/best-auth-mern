02b5e1c2fa4f2f1442b6d7852117cb73d1295252
Sameer <sameerbagwan132@gmail.com>
2023-08-27 17:32:18 +0530

    update profile

--- views/vite.config.js
+++ views/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:8080',
+        target: 'https://best-auth-mern.vercel.app',
         changeOrigin: true,
       },
     },
